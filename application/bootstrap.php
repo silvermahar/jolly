59eb815ee47b24d504309a7f3dfc95c3a3a99599
Silver Mähar <silver.mahar@khk.ee>
2013-01-19 22:57:39 +0200

    Added Main controller; Renamed Welcome to Dash; HTML template

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -105,12 +105,13 @@ Kohana::$config->attach(new Config_File);
 Kohana::modules(array( // 'auth'       => MODPATH.'auth',       // Basic authentication
     // 'cache'      => MODPATH.'cache',      // Caching with multiple backends
     // 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
-    // 'database'   => MODPATH.'database',   // Database access
+    'database' => MODPATH . 'database', // Database access
     // 'image'      => MODPATH.'image',      // Image manipulation
     // 'minion'     => MODPATH.'minion',     // CLI Tasks
     'orm' => MODPATH . 'orm', // Object Relationship Mapping
     // 'unittest'   => MODPATH.'unittest',   // Unit testing
     // 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+    'auth' => MODPATH . 'auth', // User guide and API documentation
 ));
 
 /**
@@ -119,7 +120,7 @@ Kohana::modules(array( // 'auth'       => MODPATH.'auth',       // Basic authent
  */
 Route::set('default', '(<controller>(/<action>(/<id>)))')
     ->defaults(array(
-    'controller' => 'welcome',
+    'controller' => 'dash',
     'action' => 'index',
 ));
 
---
+++ application/classes/Controller/Dash.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Dash extends Controller_Main
+{
+
+    public function action_index()
+    {
+        $this->template->content = View::factory('dash/index');
+    }
+
+} // End Welcome
